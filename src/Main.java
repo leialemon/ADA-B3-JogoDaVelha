3173860999c523a18c1ad3a98e7aede8a145f84a
leialemon <julianabarrosf@protonmail.com>
2024-07-26 22:03:57 -0300

    Novos métodos para executar tarefas de checagem.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.*;
+import java.lang.String;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[][] tabuleiro = {{"_","_","_"}, {"_","_","_"}, {"_","_","_"}};
-        boolean vitoria = false;
+        char[][] tabuleiro = {{'_','_','_'}, {'_','_','_'}, {'_','_','_'}};
+        boolean repetida;
+        boolean vitoria = checarVitoria(tabuleiro);
+        boolean empate = checarEmpate(tabuleiro);
         // Perguntar nome dos jogadores;
         System.out.println("Digite o nome do jogador X: ");
         String jogadorX = input.nextLine();
@@ -14,19 +17,53 @@ public class Main {
         String jogador0 = input.nextLine();
         String[] jogadores = {jogador0, jogadorX};
         String jogada;
-
+        int linha;
+        int coluna;
+        char marcador;
         // Mostrar placar (criar uma classe para salvar as pontuações?);
         // Escolher o primeiro jogador (aleatório? Criar alguma regra?);
 
-        do{
-            for (String jogador : jogadores) {
-                System.out.println(jogador + ", sua vez de jogar.");
-                jogada = input.nextLine();
-                tabuleiroImprimir(tabuleiro);
-                vitoria = checarVitoria(tabuleiro);
+        Random aleatorio = new Random();
+        int primeiro = aleatorio.nextInt(2);
+
+        System.out.println(jogadores[primeiro]+", você começa!");
+
+
+        while (!vitoria && !empate){
+            // loop do jogo
+            System.out.println(jogadores[primeiro]+", sua vez!");
+            jogada = input.nextLine();
+            linha = Character.getNumericValue(jogada.charAt(0));
+            coluna = Character.getNumericValue(jogada.charAt(1));
+            marcador = jogada.charAt(2);
+            repetida = checarRepetida(tabuleiro[linha][coluna]);
+            tabuleiro[linha][coluna] = marcador;
+            tabuleiroImprimir(tabuleiro);
+            vitoria = checarVitoria(tabuleiro);
+            empate = checarEmpate(tabuleiro);
+            if (Objects.equals(jogadores[primeiro], jogadorX)){
+                System.out.println(jogador0+", sua vez!");
             }
+            else {
+                System.out.println(jogadorX + ", sua vez!");
+            }
+            jogada = input.nextLine();
+            linha = Character.getNumericValue(jogada.charAt(0));
+            coluna = Character.getNumericValue(jogada.charAt(1));
+            marcador = jogada.charAt(2);
+            repetida = checarRepetida(tabuleiro[linha][coluna]);
+            tabuleiro[linha][coluna] = marcador;
+            tabuleiroImprimir(tabuleiro);
+            vitoria = checarVitoria(tabuleiro);
+            empate = checarEmpate(tabuleiro);
         }
-        while(!vitoria);
+
+        if (vitoria){
+            System.out.println("Vitória!");
+        }
+        if (empate){
+            System.out.println("Empate!");
+        }
 
         do{
             switch (input.nextLine().toLowerCase()){
@@ -40,8 +77,8 @@ public class Main {
             // Loop do jogo (método?):
             // 1 - Pedir a jogada da vez; - FEITO
             // 2 - Verificar a validade da jogada;
-            // 3 - Verificar se a jogada é igual à alguma das anteriores;
-            // 4 - Aplicar a jogada;
+            // 3 - Verificar se a jogada é igual à alguma das anteriores; (checar se o elemento tabuleiro[i][j] é diferente de "_");
+            // 4 - Aplicar a jogada; (modificar tabuleiro[i][j]);
             // 5 - Verificar se houve vencedor; - FEITO
             // 6 - Pedir a próxima jogada do outro jogador. - FEITO
         }
@@ -81,11 +118,31 @@ public class Main {
         System.out.println();
     }
 
+    // Método para verificar se a jogada é repetida;
+    public static boolean checarRepetida(char jogada){
+        boolean repetida;
+        if (jogada != '_'){
+            repetida = true;
+        }
+        else{
+            repetida = false;
+        }
+        return repetida;
+    }
+
+    public static boolean checarValidade(String jogada){
+        boolean valida = true;
+
+        //checar validade do formato da jogada.
+
+        return valida;
+    }
+
     // Método para mostrar o tabuleiro;
-    public static void tabuleiroImprimir(String[][] tabuleiro){
-        char[] coordenadas = {'A','B','C'};
+    public static void tabuleiroImprimir(char[][] tabuleiro){
+        char[] coordenadas = {'0','1','2'};
         // TABULEIRO
-        System.out.println("  A B C");
+        System.out.println("  0 1 2");
         for(int i = 0; i< tabuleiro.length; i++){
             System.out.print(coordenadas[i] + " ");
             for(int j = 0; j< tabuleiro[i].length;j++){
@@ -96,45 +153,54 @@ public class Main {
         // TABULEIRO
     }
 
+    public static boolean checarEmpate(char[][] tabuleiro) {
+        boolean empate;
+        int vazio = 0;
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                if (tabuleiro[i][j] == '_') {
+                    vazio++;
+                }
+            }
+        }
+        if (vazio == 0){
+            empate = true;
+        }
+        else {
+            empate = false;
+        }
+        return empate;
+    }
+
+
     // Método para checar se há vencedor;
-    public static boolean checarVitoria(String[][] tabuleiro){
+    public static boolean checarVitoria(char[][] tabuleiro) {
         boolean vitoria = false;
 
         // checar se os caracteres são iguais nas 8 posições que fazem a vitória. Se sim, retornar vitoria true;
         // 6 das 8 possibilidades de vitória;
-        for (int i = 0; i < tabuleiro.length; i++){
-            if (Objects.equals(tabuleiro[i][0], tabuleiro[i][1]) && Objects.equals(tabuleiro[i][0], tabuleiro[i][2])){ // condicional para dizer que vitoria é false se os caracteres iguais forem "_";
-                if (!Objects.equals(tabuleiro[i][0],"_")){
+        for (int i = 0; i < tabuleiro.length; i++) {
+            if (Objects.equals(tabuleiro[i][0], tabuleiro[i][1]) && Objects.equals(tabuleiro[i][0], tabuleiro[i][2])) {
+                if (!Objects.equals(tabuleiro[i][0], '_')) { // condicional para dizer que vitoria é false se os caracteres iguais forem "_";
                     vitoria = true;
-                    return vitoria;
+                }
+            } else if (Objects.equals(tabuleiro[0][i], tabuleiro[1][i]) && Objects.equals(tabuleiro[0][i], tabuleiro[2][i])) {
+                if (!Objects.equals(tabuleiro[0][i], '_')) {
+                    vitoria = true;
                 }
             }
-            else if (Objects.equals(tabuleiro[0][i], tabuleiro[1][i]) && Objects.equals(tabuleiro[0][i], tabuleiro[2][i])){
-                if (!Objects.equals(tabuleiro[0][i],"_")){
+
+            // 2 possibilidades restantes (diagonais);
+            if (Objects.equals(tabuleiro[0][2], tabuleiro[1][1]) && Objects.equals(tabuleiro[0][2], tabuleiro[2][0])) {
+                if (!Objects.equals(tabuleiro[0][2], '_')) {
                     vitoria = true;
-                    return vitoria;
+                }
+            } else if (Objects.equals(tabuleiro[0][0], tabuleiro[1][1]) && Objects.equals(tabuleiro[0][0], tabuleiro[2][2])) {
+                if (!Objects.equals(tabuleiro[0][0], '_')) {
+                    vitoria = true;
                 }
             }
-            else {
-                return vitoria;
-            }
         }
-
-        // 2 possibilidades restantes (diagonais);
-        if(Objects.equals(tabuleiro[0][2], tabuleiro[1][1]) && Objects.equals(tabuleiro[0][2], tabuleiro[2][0])){
-            if (!Objects.equals(tabuleiro[0][2],"_")){
-                vitoria = true;
-                return vitoria;
-            }
-        }
-
-        if (Objects.equals(tabuleiro[0][0], tabuleiro[1][1]) && Objects.equals(tabuleiro[0][0], tabuleiro[2][2])){
-            if (!Objects.equals(tabuleiro[0][0],"_")){
-                vitoria = true;
-                return vitoria;
-            }
-        }
-
         return vitoria;
     }
 }
